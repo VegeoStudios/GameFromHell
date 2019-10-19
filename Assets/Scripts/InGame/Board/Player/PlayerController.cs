32ffd834d70c645c4503f8836349ddec44ab3161
Vegeo <vegeo.studios@gmail.com>
2019-10-19 11:52:21 -0600

    Initial commit

---
+++ Assets/Scripts/InGame/Board/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public string playerName;
+    public Color color;
+    public Mesh shape;
+
+    public void OnDrawGizmos()
+    {
+        
+    }
+}
